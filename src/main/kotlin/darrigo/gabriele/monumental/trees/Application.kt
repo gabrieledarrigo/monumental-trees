0d6a490794f17cec234009c04630a981f075f74e
Gabriele D'Arrigo <darrigo.g@gmail.com>
2020-11-18 19:03:55 +0100

    Add a service definition into deployment.yml

--- src/main/kotlin/darrigo/gabriele/monumental/trees/Application.kt
+++ src/main/kotlin/darrigo/gabriele/monumental/trees/Application.kt
@@ -2,8 +2,10 @@ package darrigo.gabriele.monumental.trees
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories
 class Application
 
 fun main(args: Array<String>) {
